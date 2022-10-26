d95fe20fff50cb5ac92ea353f7a5c0c4d62295f3
renaudatascience <85571576+renauddahou@users.noreply.github.com>
2022-10-26 12:49:21 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,15 +4,15 @@ with open("README.md", "r") as fh:
     description = fh.read()
 
 setuptools.setup(
-    name="test-package",
+    name="test-package199925",
     version="0.0.1",
     author="GeeksforGeeks",
     author_email="dahou.r@yahoo.com",
-    packages=["test_package"],
+    packages=["test-package199925"],
     description="A sample test package",
     long_description=description,
     long_description_content_type="text/markdown",
-    url="https://github.com/gituser/test-tackage",
+    url="https://github.com/renauddahou/packagetest.git",
     license='MIT',
     python_requires='>=3.8',
     install_requires=[]
